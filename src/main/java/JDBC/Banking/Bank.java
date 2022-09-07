d968f20eac6b06eb814e15e664179eebce1dc850
KETANK8 <ketankumar126@gmail.com>
2022-09-07 18:09:54 +0530

    Banking project 7 sept

--- src/main/java/JDBC/Banking/Bank.java
+++ src/main/java/JDBC/Banking/Bank.java
@@ -1,3 +1,10 @@
+/**
+ * @author Ketan Kumar
+ * Illustrating BANK MANAGEMENT SYSTEM TO CREATE BANK ACCOUNT ADD ACCOUNT DETAILS TO DATABASE AND PRINT DATA OF ONE OR ALL ACCOUNT USING DATA ACCESS OBJECT
+ * PERFORMING MONEY DEPOSIT AND MONEY WITHDRAWING FROM ACCOUNT BALANCE
+ *
+ */
+
 package JDBC.Banking;
 
 import java.util.Scanner;
@@ -14,12 +21,12 @@ public class Bank {
 		int csmId,csmAge,csmAccBal,choice=0;
 		String csmName,csmPhone,csmPswrd;
 		
-		// CREATING OBJECT OF EMPLOYEEDOA
-		BankDao BDOA = new BankDao();
+		// CREATING OBJECT OF BANK DAO
+		BankDao BDAO = new BankDao();
 		
-		// CALLING COONECT METHOD TO ACCESS DATABASE
-		BDOA.connect();
-		
+		// CALLING CONNECT METHOD TO ACCESS DATABASE
+		BDAO.connect();
+
 		
 		while(choice<4) {
 		System.out.println("Press 1 - Create a Bank Account \nPress 2 - User Login \nPress 3 - Print all Bank Account Data from DataBase \nPress 4 - Exit");
@@ -29,24 +36,24 @@ public class Bank {
 		switch(choice) {
 			case 1->{
 				
-				//CREATING EMPLOYEE OBJECT TO  STORE DETAILS
+				//CREATING BANK CUSTOMER OBJECT TO  STORE DETAILS
 				BankCustomer bcsm = new BankCustomer();
 				System.out.print("Enter User Name : ");
-				csmName = scan.next();// ASKING USER TO INPUT NAME
+				csmName = scan.next();// ASKING USER TO INPUT ACCOUNT HOLDER NAME
 				bcsm.csmName = csmName;
 				System.out.print("Set Account Password : ");
-				csmPswrd = scan.next();// ASKING USER TO INPUT DEPARTMENT
+				csmPswrd = scan.next();// ASKING USER TO SET ACCOUNT PASSWORD
 				bcsm.csmPassword = csmPswrd;
 				System.out.print("Enter Age : ");
-				csmAge = scan.nextInt();// ASKING USER TO INPUT SALARY
+				csmAge = scan.nextInt();// ASKING USER TO INPUT AGE
 				bcsm.csmAge = csmAge;
 				System.out.print("Enter Phone No : ");
-				csmPhone = scan.next();// ASKING USER TO INPUT SALARY
+				csmPhone = scan.next();// ASKING USER TO INPUT PHONE NO
 				bcsm.csmPhone = csmPhone;
 				System.out.print("Enter Account Balance : ");
-				csmAccBal = scan.nextInt();// ASKING USER TO INPUT SALARY
+				csmAccBal = scan.nextInt();// ASKING USER TO ACCOUNT BALANCE
 				bcsm.csmAccBal = csmAccBal;
-				BDOA.setAccount(bcsm);// CALLING SET EMPLOYEE METHOD TO ADD DATA IN DATABASE
+				BDAO.setAccount(bcsm);// CALLING SET ACCOUNT METHOD TO CREATE BANK ACCOUNT AND ADD DATA IN DATABASE
 			}
 
 			case 2->{
@@ -55,40 +62,46 @@ public class Bank {
 				System.out.print("Enter Account Password : ");
 				csmPswrd = scan.next();
 				
-				int id = BDOA.userLogin(csmName,csmPswrd);
-				if(id>0) {
+				// CAKKING USER LOGIN METHOD TO VERIFY USER ACCOUNT
+				int id = BDAO.userLogin(csmName,csmPswrd);
+				if(id>0) { // IF ACCOUNT AND PASSWORD DETAIL CORRECT THEN USER CAN ACCESS BANK ACCOUNT
 					int cycle =0;
 					while(cycle<4) {
 						System.out.println("Press 1 - Print Account Detail \nPress 2 - Deposite Amount \nPress 3 - Withdraw Amount \nPress 4 - Log Out.");
 						System.out.println("Enter Choice : ");
 						cycle = scan.nextInt();
 						
+						// DIFFERENT TASK CAN BE PERFORM BY USER IN USER BANK ACCOUNT
 						switch(cycle) {
 						
 						case 1 ->{
-							// CALLING GET EMPLOYEE METHOD TO ACCESS AN EMPLOYEE DATA
-							BankCustomer bcsm= BDOA.getAccount(id);
-							System.out.println("Account No: "+bcsm.csmId+"  Account Holder Name: "+bcsm.csmName+"  Age: "+bcsm.csmAge+"  Phone No: "+bcsm.csmPhone+"  Account Balance: "+bcsm.csmAccBal);
+							// CALLING GET ACCOUNT METHOD TO ACCESS AN BANK ACCOUNT DETAILS
+							BankCustomer bcsm= BDAO.getAccount(id);
+							System.out.println("\nAccount No: "+bcsm.csmId+"  Account Holder Name: "+bcsm.csmName+"  Age: "+bcsm.csmAge+"  Phone No: "+bcsm.csmPhone+"  Account Balance: "+bcsm.csmAccBal);
 						}
 						
 						case 2->{
 							int amt = 0;
-							System.out.print("Enter Amount to WithDraw : ");
+							System.out.print("\nEnter Amount to Deposit : ");
 							amt = scan.nextInt();
-							BDOA.Deposit(id,amt);
+							
+							// CALLING DEPOSIT METHOD TO ADD AMOUNT IN ACCOUNT BALANCE
+							BDAO.Deposit(id,amt);
 							
 						}
 						
 						case 3->{
 							int amt = 0;
-							System.out.print("Enter Amount to WithDraw : ");
+							System.out.print("\nEnter Amount to WithDraw : ");
 							amt = scan.nextInt();
-							BDOA.withDraw(id,amt);
+							
+							// CALLING WITHDRAW METHOD TO REMOVE AMOUNT FROM USER ACCOUNT BALANCE
+							BDAO.withDraw(id,amt);
 						}
 						
 						}
 					}
-					System.out.println("Successfully Logged Out");
+					System.out.println("\nSuccessfully Logged Out\n");
 				}
 				
 			}
@@ -99,8 +112,8 @@ public class Bank {
 				System.out.print("Enter Admin Password : ");
 				csmPswrd = scan.next();
 				// CALLING GET ALL DATA
-				// TO PRITNT DATA OF EVERY EMPLOYEE FROM DATABASE
-				BDOA.getAllData(csmName,csmPswrd);
+				// TO PRITNT DATA OF EVERY USER FROM DATABASE
+				BDAO.getAllData(csmName,csmPswrd);
 			}
 		}
 		
